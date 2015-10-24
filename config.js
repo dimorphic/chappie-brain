6bbc1dcc05488020b626400c5a71f7e29820f911
Sabin Tudor <sabin@gmx.us>
2015-10-24 21:21:37 +0300

    Update app deps for Angular and clean some old files.

--- config.js
+++ config.js
@@ -20,9 +20,9 @@ CONFIG.deps = {
 
 	js: [
 		'src/vendor/requirejs/require.js',
-        'src/vendor/jquery/dist/jquery.js',
 
-		'src/vendor/react/react.js',
+        'src/vendor/jquery/dist/jquery.js',
+		'src/vendor/angular/angular.js', // YEA BABY!
 		'src/vendor/noisejs/index.js',
 
 		'src/vendor/stats.js/build/stats.min.js'
