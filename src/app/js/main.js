0e65d7a309c3d2d323a83d2183850618792b7a1a
Sabin Tudor <sabin@gmx.us>
2015-11-21 21:01:16 +0200

    Minor cleanup in main.js

--- src/app/js/main.js
+++ src/app/js/main.js
@@ -1,18 +1,16 @@
 require.config({
     baseUrl: '/js/',
 
-    // alias libraries paths.  Must set 'angular'
+    // alias libraries paths
     paths: {
 		'jquery': 'vendor/jquery',
 
-        // 'angular': '//ajax.googleapis.com/ajax/libs/angularjs/1.2.16/angular.min'
         'react': 'vendor/react',
         'noise': 'vendor/index',
 
         'statsjs': 'vendor/stats.min'
     },
 
-    // Add angular modules that does not support AMD out of the box, put it in a shim
     shim: { },
 
     // kick start application
