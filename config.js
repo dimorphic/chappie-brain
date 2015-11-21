a8f0d6cf4125aa49e96291ebacfdf3031d9be8b2
Sabin Tudor <sabin@gmx.us>
2015-11-21 20:59:51 +0200

    Add react.min (production version) to config.js

--- config.js
+++ config.js
@@ -23,6 +23,7 @@ CONFIG.deps = {
         'src/vendor/jquery/dist/jquery.js',
 
 		'src/vendor/react/react.js',
+		'src/vendor/react/react.min.js',
 		'src/vendor/noisejs/index.js',
 
 		'src/vendor/stats.js/build/stats.min.js'
